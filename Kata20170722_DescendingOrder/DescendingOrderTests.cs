88428711d9ade2c27bc437ae96a1f3357ef0d191
cash <cash@cashwu.com>
2017-07-22 22:44:09 +0800

    first test case

---
+++ Kata20170722_DescendingOrder/DescendingOrderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170722_DescendingOrder
+{
+    [TestClass]
+    public class DescendingOrderTests
+    {
+        [TestMethod]
+        public void input_21445_should_descending_order_to_54421()
+        {
+            AssertOrderShouldBe(21445, 54421);
+        }
+
+        private static void AssertOrderShouldBe(int source, int expected)
+        {
+            var kata = new Kata();
+            var acutal = kata.DescedningOrder(source);
+            Assert.AreEqual(expected, acutal);
+        }
+    }
+
+    public class Kata
+    {
+        public int DescedningOrder(int num)
+        {
+            return 54421;
+        }
+    }
+}
